eaaf58befe63ab330c0f7d6cecb7abb6e7e8240b
tehnplk <tehnplk@gmail.com>
2025-11-10 14:00:09 +0700

    feat: add version update notification script
    - Added executable script to Version.py that posts version info to Google Apps Script endpoint
    - Updated version metadata to 1.0.5 with new release date and file ID

--- Version.py
+++ Version.py
@@ -4,3 +4,19 @@ NAME = "1.0.5"
 RELEASE = "2025-11-08"
 
 __all__ = ["PROJECT", "CODE", "NAME", "RELEASE"]
+
+
+if __name__ == "__main__":
+    import requests
+    import json
+
+    url = "https://script.google.com/macros/s/AKfycbxD9CI91jGbL-MWHvBDvCqgh8s9DaTEZ38ItvX7yX37w65mQzWC_DQ5SzlrViEooh9wtg/exec"
+
+    # POST
+    payload = {
+        "new_version_code": CODE,
+        "new_version_name": NAME,
+        "new_version_release": RELEASE,
+    }
+    resp = requests.post(url, json=payload)
+    print(resp.json())
